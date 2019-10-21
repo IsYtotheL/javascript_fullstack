46c2b4b3e0e5f7975fee036f3c0ea6f4f6d3905c
18279520995@163.com <18279520995@163.com>
2019-10-21 22:09:01 +0800

    wxapp

---
+++ wxapp/didi_danche/pages/index/index.js
@@ -0,0 +1,83 @@
+Page({
+ data: {
+   markers: [],
+   longitude:116.323066, //经度
+   latitude:39.989956,//纬度
+   scale: 18
+},
+onLoad () {
+  // 微信赋予小程序可以调用的API
+  wx.showLoading({
+    title: '获取坐标中'
+    
+  });
+  //手机的GPS
+  wx.getLocation({
+    type: 'gcj02',
+    success: (res) => {
+      let { longitude, latitude } = res;
+      this.tocreateMarkers(longitude, latitude)
+     // console.log(longitude, latitude);
+     this.setData({
+       longitude,
+       latitude
+     },() => {
+       wx.hideLoading();
+         
+     })
+    }
+  })
+},
+onReady() {
+  // 地图上下文环境
+  this.mapCtx = wx.createMapContext('myMap');
+},
+toVisit (e) {
+  console.log(e);
+},
+toScan () {
+  return wx.scanCode({
+    success: (res) => {
+     // console.log(res);
+     wx.showModal({
+       title: 'scan',
+       content: JSON.stringify(res)
+     })
+    }
+  })
+},
+toUser () {
+
+},
+toMsg () {
+
+},
+toReset () {
+  // 当你使用地图来来去去的时候，再回到当初的起点
+  this.mapCtx.moveToLocation();
+  this.setData({
+    scale: 18
+  })
+
+    
+
+},
+tocreateMarkers (longitude, latitude) {
+  let markers = [
+    {
+      "id": 1,
+      "iconPath": "/images/map-bicycle.png",
+      "latitude": latitude,
+      "longitude": longitude,
+      "width": 52.5,
+      "height": 30,
+      "callout": {}
+
+  }
+]
+this.setData({
+  markers
+});
+}
+
+})
