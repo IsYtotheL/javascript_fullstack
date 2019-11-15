1fbbbdaee13a7720f167c40f319f2440f43da5ab
18279520995@163.com <18279520995@163.com>
2019-11-15 21:27:29 +0800

    vue

---
+++ vue/vue-communication/src/components/view/demo1/child.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="child">
+    <h1>子组件</h1>
+    <p>The Msg from father is:</p>
+    <strong>{{sendMsg}}</strong>
+
+    <p class="hr"></p>
+
+    <p>The ChangeMsg from child is:</p>
+    <strong>{{changeMsg}}</strong>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['sendMsg'],
+  computed: {
+    changeMsg () {
+      return this.sendMsg.toUpperCase()
+    }
+  }
+}
+</script>
+
+<style>
+.hr{
+  margin: 50px 0;
+}
+</style>
---
+++ vue/vue-communication/src/components/view/demo2/child.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="child">
+    <h2>子组件</h2>
+    <p>从父组件接受到的信息</p>
+  <div>
+    String:<span class="red">{{sendMsg}}</span>
+    </div>
+    <div>
+    Number:<span class="red">{{sendNum}}</span>
+   </div>
+
+   <ul class="red">
+      <li v-for="item in myObj" :key="item.id">{{item.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    sendMsg: {
+      type: String,
+      required: true
+    },
+    sendNum: {
+      type: Number,
+      required: false
+    },
+    sendObj: {
+      // 声明验证类型
+      validator (val) {
+        if (Object.prototype.toString.call(val) === "[object Array]" ||
+        Object.prototype.toString.call(val) === "[object object]"
+        ) {
+          return true
+        }
+      }
+    }
+  }
+
+}
+</script>
+
+<style>
+.red{
+  color: red;
+}
+</style>
