6040028ca9e0b7f6828935c14d6051ab99cad43e
18279520995@163.com <18279520995@163.com>
2019-12-04 20:06:34 +0800

    notebook

---
+++ vue/nootbook/noot_book/controllers/defaultConfig.js
@@ -0,0 +1,12 @@
+const config = {
+  database: {
+    DATABASE: 'test',
+    USERNAME: 'root',
+    PASSWORD:'XFWXN1023',
+    PORT:'3306',
+    HOST:'localhost'
+
+  }
+}
+
+module.exports = config
